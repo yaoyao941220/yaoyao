4b6aa2eeec6dfd686df4eea5e3000f13b753e081
YSD <1351068640@qq.com>
2017-04-16 14:46:36 +0800

    支付

---
+++ Order.js
@@ -0,0 +1,30 @@
+/**
+ * Created by YSD on 2017/4/15.
+ */
+var mydiv2=document.getElementById("mydiv2");
+var cli=document.getElementById("cli");
+
+function show() {
+    mydiv2.style.display="block";
+}
+
+var del=document.getElementById("del");
+function hidden() {
+    mydiv2.style.display=none;
+}
+var baocun=document.getElementById("baocun");
+var clii=document.getElementById("clii");
+function show1() {
+    mydiv2.style.display="none";
+    clii.style.display="block";
+}
+var a1=document.getElementById(a1);
+var mydiv2=document.getElementById("mydiv2");
+function show2() {
+    mydiv2.style.display="block";
+}
+var a2=document.getElementById("a2");
+var clii=document.getElementById("clii");
+function show3() {
+    clii.style.display="none";
+}
